1030825ddc9e34bbeb2ecabd569fc8911782edd2
tickelton <tickelton@gmail.com>
2020-08-16 11:27:51 +0200

    pyautogui and suprocess POC working

--- foozzer.py
+++ foozzer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
+import subprocess
+import pyautogui
 from time import sleep
 from subprocess import PIPE, STDOUT, Popen
 from threading  import Thread
@@ -10,8 +12,38 @@ from queue import Queue, Empty
 VALGRIND = '/usr/bin/valgrind'
 XTERM = '/usr/bin/xterm'
 LS = '/usr/bin/ls'
+NEW_PLAYLIST = r'D:\Workspace\foozzer\images\new_playlist.png'
+RM_PL = r'D:\Workspace\foozzer\images\remove_playlist.png'
+TITLE_INFORMATION = r'D:\Workspace\foozzer\images\information.png'
+PL_GARBAGE = r'D:\Workspace\foobar_fuzzing\in\garbage.fpl'
+PL_GENERIC = r'D:\Workspace\foobar_fuzzing\in\generic.fpl'
+FOOBAR = r'C:\Program Files (x86)\foobar2000\foobar2000.exe'
+CMD_STOP = '/stop'
 ON_POSIX = 'posix' in sys.builtin_module_names
 
+
+def cmd_stop():
+    subprocess.run([FOOBAR, CMD_STOP])
+
+def pl_generic():
+    subprocess.run([FOOBAR, PL_GENERIC])
+
+def pl_garbage():
+    subprocess.run([FOOBAR, PL_GARBAGE])
+
+def del_new_pl():
+    new_pl = pyautogui.locateOnScreen(NEW_PLAYLIST)
+    if new_pl:
+        new_pl_center = pyautogui.center(new_pl)
+        pyautogui.click(button='right', x=new_pl_center.x, y=new_pl_center.y)
+        del_pl = pyautogui.locateOnScreen(RM_PL)
+        del_pl_center = pyautogui.center(del_pl)
+        pyautogui.click(x=del_pl_center.x, y=del_pl_center.y)
+
+def close_info():
+    win_info = pyautogui.locateOnScreen(TITLE_INFORMATION)
+    if win_info:
+        pyautogui.click(x=win_info.left+win_info.width-5, y=win_info.top+5)
 
 def enqueue_output(out, queue):
     for line in iter(out.readline, b''):
