6502504fc1f73d5b5940b5dc2bf7242f7998851b
tickelton <tickelton@gmail.com>
2020-09-18 22:11:20 +0200

    Adds dword mutator
    The dword mutator demonstrates how to change individual
    32 bit values in an input file.

--- foozzer/mutators/dword.py
+++ foozzer/mutators/dword.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """
-Example mutator to be used specifically for fuzzing Foobar2000 playlists.
+Example mutator for testing individual 32 bit values.
 
 The mutator appends 1k of garbage data to the input file and modifies
-an single byte of the original data per iteration.
+an single 32bit value of the original data per iteration.
 """
 
 # Copyright (c) 2020 tick <tickelton@gmail.com>
@@ -13,18 +13,18 @@ import os
 import configparser
 import logging
 
-MUTATOR_NAME = 'fpl_basic'
+MUTATOR_NAME = 'dword'
 STATE_FILE_NAME = MUTATOR_NAME + '_state.txt'
-OUTFILE_NAME = 'fuzz_pl.fpl'
+OUTFILE_NAME = 'dword.out'
 
 log = logging.getLogger(__name__)
 
 def get_module_info():
     """Returns data used by main function to discover plugins."""
 
-    return {MUTATOR_NAME: ('Modifies individual bytes in the input file', FPLBasicMutator)}
+    return {MUTATOR_NAME: ('Iterates through file in 32 bit increments', DWORDMutator)}
 
-class FPLBasicMutator:
+class DWORDMutator:
     """
     The mutator class works as an iterator that produces a mutation
     of the input file on every iteration.
@@ -33,10 +33,14 @@ class FPLBasicMutator:
     _GARBAGE_PATTERN = b'Aa0Aa1Aa2Aa3Aa4Aa5Aa6Aa7Aa8Aa9Ab0Ab1Ab2Ab3Ab4Ab5Ab6Ab7Ab8Ab9Ac0Ac1Ac2Ac3Ac4Ac5Ac6Ac7Ac8Ac9Ad0Ad1Ad2Ad3Ad4Ad5Ad6Ad7Ad8Ad9Ae0Ae1Ae2Ae3Ae4Ae5Ae6Ae7Ae8Ae9Af0Af1Af2Af3Af4Af5Af6Af7Af8Af9Ag0Ag1Ag2Ag3Ag4Ag5Ag6Ag7Ag8Ag9Ah0Ah1Ah2Ah3Ah4Ah5Ah6Ah7Ah8Ah9Ai0Ai1Ai2Ai3Ai4Ai5Ai6Ai7Ai8Ai9Aj0Aj1Aj2Aj3Aj4Aj5Aj6Aj7Aj8Aj9Ak0Ak1Ak2Ak3Ak4Ak5Ak6Ak7Ak8Ak9Al0Al1Al2Al3Al4Al5Al6Al7Al8Al9Am0Am1Am2Am3Am4Am5Am6Am7Am8Am9An0An1An2An3An4An5An6An7An8An9Ao0Ao1Ao2Ao3Ao4Ao5Ao6Ao7Ao8Ao9Ap0Ap1Ap2Ap3Ap4Ap5Ap6Ap7Ap8Ap9Aq0Aq1Aq2Aq3Aq4Aq5Aq6Aq7Aq8Aq9Ar0Ar1Ar2Ar3Ar4Ar5Ar6Ar7Ar8Ar9As0As1As2As3As4As5As6As7As8As9At0At1At2At3At4At5At6At7At8At9Au0Au1Au2Au3Au4Au5Au6Au7Au8Au9Av0Av1Av2Av3Av4Av5Av6Av7Av8Av9Aw0Aw1Aw2Aw3Aw4Aw5Aw6Aw7Aw8Aw9Ax0Ax1Ax2Ax3Ax4Ax5Ax6Ax7Ax8Ax9Ay0Ay1Ay2Ay3Ay4Ay5Ay6Ay7Ay8Ay9Az0Az1Az2Az3Az4Az5Az6Az7Az8Az9Ba0Ba1Ba2Ba3Ba4Ba5Ba6Ba7Ba8Ba9Bb0Bb1Bb2Bb3Bb4Bb5Bb6Bb7Bb8Bb9Bc0Bc1Bc2Bc3Bc4Bc5Bc6Bc7Bc8Bc9Bd0Bd1Bd2Bd3Bd4Bd5Bd6Bd7Bd8Bd9Be0Be1Be2Be3Be4Be5Be6Be7Be8Be9Bf0Bf1Bf2Bf3Bf4Bf5Bf6Bf7Bf8Bf9Bg0Bg1Bg2Bg3Bg4Bg5Bg6Bg7Bg8Bg9Bh0Bh1Bh2Bh3Bh4Bh5Bh6Bh7Bh8Bh9Bi0B'
 
     _byte_mods = [
-        b'\x00',
-        b'\x01',
-        b'\x41',
-        b'\xff',
+        b'\x00\x00\x00\x00',
+        b'\x00\x00\x00\x01',
+        b'\x01\x00\x00\x00',
+        b'\x41\x00\x00\x00',
+        b'\x00\x00\x00\x41',
+        b'\xff\xff\x00\x00',
+        b'\x00\x00\xff\xff',
+        b'\xff\xff\xff\xff',
     ]
 
     def __init__(self, indir, outdir):
